f6b6d9824f4d3b6b5b8573535daecc704f4b4fd2
chenxy <15101128707@163.com>
2016-10-10 09:18:27 +0800

    [chenxy]添加发送邮件依赖的jar包。

---
+++ src/com/chenxy/java1/Java1Prac10.java
@@ -0,0 +1,18 @@
+package com.chenxy.java1;
+
+/**
+ * @version 1.0
+ * @since 1.0
+ * @see com.chenxy.java1.Java1Prac10
+ * @author admin
+ * @desc  使用java发送邮件非常简单：
+ *<p>
+ *     机器上安装javaMailAPI 与 JavaActivationFramework (JAF)
+ *     将jar包添加到classPath 中
+ *</p>
+ *
+ */
+public class Java1Prac10 {
+
+
+}
