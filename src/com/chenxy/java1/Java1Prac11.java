5c80ada78e8883ad5ec8a1d28afa43e35576fd7c
chenxy <15101128707@163.com>
2016-10-12 14:09:30 +0800

    [chenxy]字符串的compareTo 与unicode 编码。

---
+++ src/com/chenxy/java1/Java1Prac11.java
@@ -0,0 +1,45 @@
+package com.chenxy.java1;
+
+/**
+ * @version 1.0
+ * @author admin
+ * @see java.lang.String
+ * @<p>
+ *     String a="";
+ *     a.compareTo("");
+ * </p>
+ */
+public class Java1Prac11 implements Comparable
+{
+
+
+    /**
+     * <p>
+     *     字符串的compareTo方法：
+     *     1 所有实现Comparable 接口的类都可以使用
+     *     2 原理：比较米一个字符的unicode编码后的值是否相等
+     *     3 与equals 在比较两个字符串相等的时候效果一样
+     *     4 返回值时int ==0 时相等
+     *
+     * </p>
+     * @param args
+     */
+    public static void main(String[] args){
+        String str="Hello World";
+        String str1="hello world";
+        System.out.println(str.compareTo(str1));
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        //
+        return 0;
+    }
+
+    /**
+     * 1
+     */
+    public void unicode(){
+
+    }
+}
