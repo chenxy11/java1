c905a56681b3c984c1f76329485110677e5ad40c
chenxy <15101128707@163.com>
2016-10-25 09:19:16 +0800

    [chenxy数据库得链接信息。

--- gather/src/com/chenxy/utils/common/OracleUtils.java
+++ gather/src/com/chenxy/utils/common/OracleUtils.java
@@ -1,6 +1,8 @@
 package com.chenxy.utils.common;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 /**
@@ -11,17 +13,30 @@ public abstract class OracleUtils {
     public static String username= null;
     public static String password=null;
     public static String conUrl=null;
-
+    public static String driver=null;
+    public static Connection connection=null;
     static {
           username= ResourceBundle.getBundle("oracle").getString("username");
           password=ResourceBundle.getBundle("oracle").getString("password");
           conUrl=ResourceBundle.getBundle("oracle").getString("url");
-
+          driver=ResourceBundle.getBundle("oracle").getString("driver");
     }
     public static Connection getConnection(){
-
-        return null;
-    }
+        try {
+            if(connection==null){
+                Class.forName(driver);
+                return DriverManager.getConnection(conUrl,username,password);
+            }else{
+                return connection;
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+     }
 
 
 }
